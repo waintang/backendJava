09af4082b9164e2cf8696cf0bec565da2be4de0a
waintang <47218973+waintang@users.noreply.github.com>
2021-09-13 11:06:59 +0800

    位运算符

--- src/test/java/com/example/practice/algorithm/BitOperationTest.java
+++ src/test/java/com/example/practice/algorithm/BitOperationTest.java
@@ -1,5 +1,8 @@
 package com.example.practice.algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BitOperationTest {
 
     /**
@@ -8,11 +11,49 @@ public class BitOperationTest {
      * @param args
      */
     // 参考链接：https://blog.csdn.net/xiaochunyong/article/details/7748713      Java 位运算(移位、位与、或、异或、非）
+    // 应用：hashmap的泊松分布、加载因子关系：https://blog.csdn.net/liji_xc/article/details/79698223  java集合框架分析-HashMap(加载因子及初始容量深入分析)
     public static void main(String[] args) {
-        Integer a = -8;
-        // 二进制
-        System.out.println( Integer.toBinaryString(a));
-        System.out.println(~a);
-        System.out.println(Integer.toBinaryString(~a));
+        Integer a = 57;
+        //左移
+        System.out.println("将5左移2位:");
+        System.out.println( Integer.toBinaryString(5));
+        System.out.println( 5<<2);
+        System.out.println( Integer.toBinaryString(5<<2));
+        //右移
+        System.out.println("将5右移2位:");
+        System.out.println( Integer.toBinaryString(5));
+        System.out.println( 5>>2);
+        System.out.println( Integer.toBinaryString(5>>2));
+        //无符号右移
+        System.out.println("将-5无符号右移2位:");
+        System.out.println( Integer.toBinaryString(-5));
+        System.out.println( 5>>>2);
+        System.out.println( Integer.toBinaryString(-5>>>2));
+
+        // 与
+        System.out.println("5&3:");
+        System.out.println( Integer.toBinaryString(5));
+        System.out.println( Integer.toBinaryString(3));
+        System.out.println(5&3);
+        // 或
+        System.out.println("5|3:");
+        System.out.println( Integer.toBinaryString(5));
+        System.out.println( Integer.toBinaryString(3));
+        System.out.println(5|3);
+        // 非
+        System.out.println("~5:");
+        System.out.println( Integer.toBinaryString(5));
+        System.out.println( Integer.toBinaryString(~5));
+        // 异或
+        System.out.println("5^3:");
+        System.out.println( Integer.toBinaryString(5));
+        System.out.println( Integer.toBinaryString(3));
+        System.out.println(5^3);
+
+        // 0  -1的二进制
+        System.out.println("0:");
+        System.out.println( Integer.toBinaryString(0));
+        System.out.println("-1:");  //算法 ： 1原码的反码 + 1
+        System.out.println( Integer.toBinaryString(-1));
     }
 }
---
+++ src/test/java/com/example/practice/pattern/concurrency/reactor/single_multi/SingleReactMultiThreadTest.java
@@ -0,0 +1,7 @@
+package com.example.practice.pattern.concurrency.reactor.single_multi;
+
+/**
+ * reactor模型的三种模式：单Reactor单线程、单Reactor多线程、主从Reactor多线程
+ */
+public class SingleReactMultiThreadTest {
+}
