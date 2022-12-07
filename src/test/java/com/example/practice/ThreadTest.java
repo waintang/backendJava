2b7027674f2ef29a995e2dd484b3f00e9bde7d5a
waintang <47218973+waintang@users.noreply.github.com>
2022-12-07 19:57:53 +0800

    线程Thread中断（原理）+juc的AQS抽象队列同步器队列/Condition条件队列

--- src/test/java/com/example/practice/ThreadTest.java
+++ src/test/java/com/example/practice/ThreadTest.java
@@ -7,10 +7,39 @@ import java.util.Random;
 public class ThreadTest {
     public static ThreadLocal<Map<String,Object>> threadLocal = new ThreadLocal<>();
     public static void main(String[] args) {
+        // ########### Thread.interrupt中断、Thread.interrupted判断是否中断
+        System.out.println("开始测试：Thread.interrupt中断、Thread.interrupted判断是否中断");
+        InterruptRunnable interruptRunnable = new InterruptRunnable();
+        Thread interruptThread = new Thread(interruptRunnable);
+        interruptThread.start();
+        try {
+            System.out.println("开始睡眠");
+            Thread.sleep(1000);
+            System.out.println("结束睡眠");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        interruptThread.interrupt();
+        System.out.println("结束测试：Thread.interrupt中断、Thread.interrupted判断是否中断");
+        // ########### threadLocal
         TwpThread twpThread1 = new TwpThread();
         TwpThread twpThread2 = new TwpThread();
         new Thread(twpThread1).start();
         new Thread(twpThread2).start();
+    }
+
+    public static class InterruptRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            while(!Thread.interrupted()){
+                System.out.println(1);
+            }
+            System.out.println("退出循环");
+            for(int i = 0;i<100;i++){
+                System.out.println(i);
+            }
+        }
     }
 
     public static class TwpThread implements Runnable{
--- src/test/java/com/example/practice/juc/JucTest.java
+++ src/test/java/com/example/practice/juc/JucTest.java
@@ -2,12 +2,25 @@ package com.example.practice.juc;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class JucTest {
+
+    private static AtomicLong atomicLong = new AtomicLong();
     BlockingQueue blockingQueue = new ArrayBlockingQueue(10);
+    // AQS同步队列：https://blog.csdn.net/weixin_43935927/article/details/113901484
     Lock lock = new ReentrantLock();
+    // 条件队列：https://blog.csdn.net/qq_33762302/article/details/114381258
     Condition notFull = lock.newCondition();
+
+    public static void main(String[] args) {
+        System.out.println(atomicLong);
+        long updatedLong = atomicLong.addAndGet(-1); // 返回-1
+        long previousLong = atomicLong.getAndAdd(-1); // 返回-1
+        System.out.println(atomicLong); // 返回-2
+
+    }
 }
