58348cc58cdf9ae3526752520e377aaef5e97f72
waintang <47218973+waintang@users.noreply.github.com>
2024-08-15 02:40:54 +0800

    try-finally

---
+++ src/test/java/com/example/practice/FinallyTest.java
@@ -0,0 +1,13 @@
+package com.example.practice;
+
+public class FinallyTest {
+    public static void main(String[] args) {
+        try {
+            System.out.println("try一");
+            throw new RuntimeException("抛出异常");
+//            System.out.println("thorw 后");
+        }finally {
+            System.out.println("finally一");
+        }
+    }
+}
